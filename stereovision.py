9ac055f9cc5d03ed26d6c3ebbaaf22ff7d1f5308
madperez <madperez@hotmail.com>
2022-03-20 02:28:07 -0600

    iniciando estereo

---
+++ stereovision.py
@@ -0,0 +1,13 @@
+from email.errors import InvalidMultipartContentTransferEncodingDefect
+import matplotlib.pyplot as plt
+from PIL import Image
+class stereo():
+    def __init__(self,imleft,imright,dmax=15) -> None:
+        self.iml=imleft 
+        self.imr=imright 
+        self.dmax=dmax
+    def show():
+        plt.subplot(1,2,1)
+
+iml=Image.op('tsukubal.png')
+        
